2c50d5f0c878061423a9362a42a72e368d3d6013
ruben-iteng <94007802+ruben-iteng@users.noreply.github.com>
2024-08-13 16:47:57 +0200

    Add annotation to typer options; Add exporters

--- src/TEMPLATE_VAR_project_name/main.py
+++ src/TEMPLATE_VAR_project_name/main.py
@@ -13,19 +13,11 @@ from pathlib import Path
 import faebryk.libs.picker.lcsc as lcsc
 import typer
 from faebryk.core.util import get_all_modules
-from faebryk.exporters.bom.jlcpcb import (
-    convert_kicad_pick_and_place_to_jlcpcb,
-    write_bom_jlcpcb,
-)
-from faebryk.exporters.esphome.esphome import make_esphome_config
-from faebryk.exporters.pcb.kicad.artifacts import (
-    export_dxf,
-    export_gerber,
-    export_glb,
-    export_pick_and_place,
-    export_step,
-)
+from faebryk.exporters.esphome.esphome import dump_esphome_config, make_esphome_config
+from faebryk.exporters.parameters.parameters_to_file import export_parameters_to_file
+from faebryk.exporters.pcb.kicad.artifacts import export_svg
 from faebryk.libs.app.checks import run_checks
+from faebryk.libs.app.manufacturing import export_pcba_artifacts
 from faebryk.libs.app.parameters import replace_tbd_with_any
 from faebryk.libs.app.pcb import apply_design
 from faebryk.libs.logging import setup_basic_logging
@@ -34,27 +26,40 @@ from faebryk.libs.picker.picker import pick_part_recursively
 from TEMPLATE_VAR_project_name.app import MyApp
 from TEMPLATE_VAR_project_name.pcb import transform_pcb
 from TEMPLATE_VAR_project_name.pickers import add_app_pickers
+from typing_extensions import Annotated
 
 # logging settings
 logger = logging.getLogger(__name__)
 
 
 def main(
-    export_pcba_artifacts: bool = False,
-    export_esphome_config: bool = False,  # remove if not using esphome
+    export_manufacturing_artifacts: Annotated[
+        bool, typer.Option(help="Export manufacturing artifacts (gerbers, BOM, etc.)")
+    ] = False,
+    export_esphome_config: Annotated[
+        bool, typer.Option(help="Export ESPHome config yaml")
+    ] = False,
+    export_visuals: Annotated[
+        bool, typer.Option(help="Export project visuals (e.g. SVG)")
+    ] = False,
+    export_parameters: Annotated[
+        bool, typer.Option(help="Export project parameters to a file")
+    ] = False,
 ):
     # paths --------------------------------------------------
+    root = Path(__file__).parent.parent.parent
+    kicad_prj_path = root.joinpath("source")
+    pcbfile = kicad_prj_path.joinpath("main.kicad_pcb")
     build_dir = Path("./build")
     faebryk_build_dir = build_dir.joinpath("faebryk")
     faebryk_build_dir.mkdir(parents=True, exist_ok=True)
-    root = Path(__file__).parent.parent.parent
     netlist_path = faebryk_build_dir.joinpath("faebryk.net")
-    kicad_prj_path = root.joinpath("source")
-    pcbfile = kicad_prj_path.joinpath("main.kicad_pcb")
     esphome_path = build_dir.joinpath("esphome")
     esphome_config_path = esphome_path.joinpath("esphome.yaml")
     manufacturing_artifacts_path = build_dir.joinpath("manufacturing")
-    cad_path = build_dir.joinpath("cad")
+    parameter_dir = build_dir.joinpath("parameters")
+    parameters_path = parameter_dir.joinpath("parameters.md")  # .txt is also possible
+    visuals_dir = build_dir.joinpath("visuals")
 
     lcsc.BUILD_FOLDER = build_dir
     lcsc.LIB_FOLDER = root.joinpath("libs")
@@ -86,31 +91,24 @@ def main(
     apply_design(pcbfile, netlist_path, G, app, transform_pcb)
 
     # generate pcba manufacturing and other artifacts ---------
-    if export_pcba_artifacts:
-        logger.info("Exporting PCBA artifacts")
-        write_bom_jlcpcb(
-            get_all_modules(app),
-            manufacturing_artifacts_path.joinpath("jlcpcb_bom.csv"),
-        )
-        export_step(pcbfile, step_file=cad_path.joinpath("pcba.step"))
-        export_glb(pcbfile, glb_file=cad_path.joinpath("pcba.glb"))
-        export_dxf(pcbfile, dxf_file=cad_path.joinpath("pcba.dxf"))
-        export_gerber(
-            pcbfile, gerber_zip_file=manufacturing_artifacts_path.joinpath("gerber.zip")
-        )
-        pnp_file = manufacturing_artifacts_path.joinpath("pick_and_place.csv")
-        export_pick_and_place(pcbfile, pick_and_place_file=pnp_file)
-        convert_kicad_pick_and_place_to_jlcpcb(
-            pnp_file,
-            manufacturing_artifacts_path.joinpath("jlcpcb_pick_and_place.csv"),
-        )
+    if export_manufacturing_artifacts:
+        export_pcba_artifacts(manufacturing_artifacts_path, pcbfile, app)
+
+    # generate visuals ---------------------------------------
+    if export_visuals:
+        export_svg(pcbfile, visuals_dir.joinpath("pcba.svg"))
+
+    # export parameter report --------------------------------
+    if export_parameters:
+        export_parameters_to_file(app, parameters_path)
 
     # esphome config -----------------------------------------
-    # Remove if not using esphome
     if export_esphome_config:
         logger.info("Generating esphome config")
         esphome_config = make_esphome_config(G)
-        esphome_config_path.write_text(esphome_config, encoding="utf-8")
+        esphome_config_path.write_text(
+            dump_esphome_config(esphome_config), encoding="utf-8"
+        )
 
 
 if __name__ == "__main__":
