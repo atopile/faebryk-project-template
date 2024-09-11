3ae1ba20cd286312c5c408885e7b490c7575d19e
Ioannis P <ioannis@iteng.nl>
2024-09-11 10:19:26 +0200

    Fix: Deps, imports and leftover LL
    Deps: Yaml
    Imports: F, L & P
    Leftover LL: IFs

--- src/TEMPLATE_VAR_project_name/library/my_library_module.py
+++ src/TEMPLATE_VAR_project_name/library/my_library_module.py
@@ -3,7 +3,10 @@
 
 import logging
 
+import faebryk.library._F as F  # noqa: F401
 from faebryk.core.module import Module
+from faebryk.libs.library import L  # noqa: F401
+from faebryk.libs.units import P  # noqa: F401
 
 logger = logging.getLogger(__name__)
 
--- src/TEMPLATE_VAR_project_name/main.py
+++ src/TEMPLATE_VAR_project_name/main.py
@@ -12,7 +12,7 @@ from pathlib import Path
 
 import faebryk.libs.picker.lcsc as lcsc
 import typer
-from faebryk.core.util import get_all_modules
+from faebryk.core.module import Module
 from faebryk.exporters.esphome.esphome import dump_esphome_config, make_esphome_config
 from faebryk.exporters.parameters.parameters_to_file import export_parameters_to_file
 from faebryk.exporters.pcb.kicad.artifacts import export_svg
@@ -73,7 +73,9 @@ def main(
 
     # pick parts ---------------------------------------------
     logger.info("Picking parts")
-    modules = {n.get_most_special() for n in get_all_modules(app)}
+    modules = {
+        n.get_most_special() for n in app.get_children(direct_only=False, types=Module)
+    }
     for n in modules:
         add_jlcpcb_pickers(n, base_prio=10)
         add_app_pickers(n)
